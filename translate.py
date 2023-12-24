d96d320d055bf8574fd0701c1ad22227cad6f82c
Ruthik <147206849+Ruthikr@users.noreply.github.com>
2023-12-24 22:38:17 +0530

    Update translate.py

--- translate.py
+++ translate.py
@@ -149,10 +149,10 @@ if b:
     output_update=pd.DataFrame({"Language": [lang], "text": [answer["data"]["translatedText"]],})
     new_df=pd.concat([output_df,output_update], ignore_index=True)
     new_df.to_csv("output_df.csv", index=False)
-d=st.button("data")
-if d:
-    st.dataframe(df)
-    st.dataframe(new_df)
+    d=st.button("data")
+    if d:
+        st.dataframe(df)
+        st.dataframe(new_df)
    
         
 
