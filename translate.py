70d623f1bdb6901b29425ab5541462c895389c62
Ruthik <147206849+Ruthikr@users.noreply.github.com>
2023-12-24 22:53:25 +0530

    Update translate.py

--- translate.py
+++ translate.py
@@ -150,7 +150,9 @@ try:
         output_update=pd.DataFrame({"Language": [lang], "text": [answer["data"]["translatedText"]],})
         new_df=pd.concat([output_df,output_update], ignore_index=True)
         new_df.to_csv("output_df.csv", index=False)
-        st.write(df)
-        st.write(new_df)
+        d=st.button("table")
+        if d:
+            st.write(df)
+            st.write(new_df)
 except:
     st.write("")
